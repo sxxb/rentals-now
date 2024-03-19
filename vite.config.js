b34e0f5ae0b08c8ee2314b1977df7e8c17550454
Scott Belcher <scott@wavecom.com.au>
2024-03-19 12:43:01 +1030

    third try at config for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rentals-now/',
+  base: "./rentals-now/",
   plugins: [react()],
   resolve: {
     alias: {
