32316489beaba371dfe7024811324afcf361e9f8
Scott Belcher <scott@wavecom.com.au>
2024-03-19 13:40:16 +1030

    7th config attempt

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://sxxb.github.io/rentals-now/",
+  base: 'https://sxxb.github.io/rentals-now/',
   plugins: [react()],
   resolve: {
     alias: {
